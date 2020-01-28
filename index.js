c67fde769e3d86edc764db4089ed7bca366c36d9
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-01-28 16:19:41 -0300

    desafio 1 - api crud de projetos e tarefas

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+
+const PORT = 3000;
+
+let projects = [];
+let totalRequests = 0;
+
+const server = express();
+server.use(express.json());
+server.use((req, res, next) => {
+  totalRequests++;
+  console.log(`Total de requisições feitas a API: ${totalRequests}`);
+  return next();
+});
+
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+
+  const project = projects.filter(project => project.id === id);
+
+  if (project.length === 0) {
+    return res.status(400).json({ message: "Project does not exist" });
+  }
+
+  return next();
+}
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = { id, title, tasks: [] };
+
+  projects.push(project);
+
+  return res.json(projects);
+});
+
+server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projects.map(project => {
+    if (project.id === id) {
+      project.tasks.push(title);
+    }
+  });
+
+  return res.json(projects);
+});
+
+server.put("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projects.map(project => {
+    if (project.id === id) {
+      project.title = title;
+    }
+    return project;
+  });
+
+  return res.json(projects);
+});
+
+server.delete("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  projects = projects.filter(project => project.id !== id);
+
+  return res.send();
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
